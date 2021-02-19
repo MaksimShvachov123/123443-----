ec4fa2f42daee9c2c2fa5ac0d0256ae6d9e348b5
MaksimShvachov123 <sashakidalo@ukr.net>
2021-02-19 19:58:45 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+  //const form = {
+      // id: 3,
+      // name: "Maksim",
+      //lastName: "Shvachov",
+      //Age: "13 years",
+      //Height: "1m 65cm",
+      //Place: "Kyiv",
+      //email:"mshvachev@ukr.net"
+  //}
+
+
+  //const arrayOfKeys = object.keys(obj)
+
+  //console.log(form)
+  //console.log(typeof form)
+
+  //const jsonForRequest = JSON.stringify(form)
+
+
+  //console.log(jsonForRequest)
+  //console.log(typeof jsonForRequest)
+
+ // const objectFromJson = JSON.parse(jsonForRequest)
+
+
+  //console.log(objectFromJson)
+  //console.log(typeof objectFromJson)
+
